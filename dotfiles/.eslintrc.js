60f6ccd0f6c8096e1fb9462bc3aa95b4d783c0ef
railek <jean@railek.com>
2020-06-14 22:03:16 +0200

    feat: add .eslintrc.js & .eslintignore

---
+++ dotfiles/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {},
+  extends: 'prettier',
+  parserOptions: {},
+  plugins: ['prettier', 'import'],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'prettier/prettier': ['error'],
+  },
+};
